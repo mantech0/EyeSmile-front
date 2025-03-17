234cca74c1033f5070e90733cc743699285774ed
mantech0 <man0519tech@gmail.com>
2025-03-18 00:43:23 +0900

    fix: update Azure Web Apps deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     sourcemap: false,
     rollupOptions: {
       output: {
@@ -15,6 +13,6 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 }) 
